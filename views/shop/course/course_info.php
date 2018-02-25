e59660b9aadcfba224ab99b76d5ae637dce92f40
Administrator <becwal@yandex.ru>
2018-02-25 15:39:06 +0300

    Подробный course с уроками lessons-небольшой редизайн

--- views/shop/course/course_info.php
+++ views/shop/course/course_info.php
@@ -15,11 +15,30 @@ use yii\helpers\Url;
         <div class="thumbnail" style="height: 29em" >
             <img src="https://www.videosharp.info/data/img/shop/<?= $course->courl ?>.png" alt="game2" style="height: 27em">
         </div>
-        <div class="btn-group btn-group-justified">
-            <?php
-            $course_url = Url::to(['/shop/course/', 'id' => $course->id]);
-            ?>
-            <a href="<?= $course_url ?>" title="Подробнее о курсе" class="btn btn-success">
+
+        <div>
+            <div class="list-group">
+                 <a href="#" class="list-group-item active">
+                        <span class="glyphicon glyphicon-star"></span>&nbsp;&nbsp 
+                         Наименование урока
+                        <span class="badge">Длительность</span>
+                    </a>
+                <?php foreach ($course->lesson as $lesson) {
+                    ?>
+
+                    <a href="#" class="list-group-item">
+                        <span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp 
+                         <?=$lesson->lesson?>
+                        <span class="badge"><?=$lesson->video_length?></span>
+                    </a>
+                    <?}?>
+                </div>
+            </div>
+            <div class="btn-group btn-group-justified">
+                <?php
+                $course_url = Url::to(['/shop/course/', 'id' => $course->id]);
+                ?>
+                <a href="<?= $course_url ?>" title="Подробнее о курсе" class="btn btn-success">
                 <i class="glyphicon glyphicon-chevron-right">
 
                 </i>
